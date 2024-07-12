1e0a448d9c8e3e67d95b7995c31e5bf69e7f81fd
viniciuseduuu <viniciuseduardohilario@gmail.com>
2024-07-12 16:49:51 -0300

    a

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import expandbox from './components/expandbox.vue';
+</script>
+
+<template>
+<h1>Componentes</h1>
+<expandbox />
+<expandbox />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/expandbox.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const showContent = ref(false)
+</script>
+
+<template>
+    <div>
+        <button @click="showContent = !showContent">Mostrar</button>
+            <div v-if="showContent == true" class="expandbox">aqui vai um texto escondido</div>
+    </div>
+</template>
+
+<style scoped>
+
+.expandbox {
+    height: 100%;
+    padding: 20px;
+    border: 1px solid black;
+    box-shadow: 2px 2px 3px 3px gray;
+    border-radius: 10px;
+}
+</style>
